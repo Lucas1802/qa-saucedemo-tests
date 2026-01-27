b4791b566d9f9147d671a61517f1e41deadcbe80
Lucas Mendes <50122189+Lucas1802@users.noreply.github.com>
2026-01-26 23:42:44 -0300

    feat: estrutura inicial de QA com Cypress e POM

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://www.saucedemo.com",
+    specPattern: "automacao-tests/cypress/e2e/**/*.cy.js",
+    supportFile: "automacao-tests/cypress/support/e2e.js",
+  },
+});
